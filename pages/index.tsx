8458a7456c73908ba25bf16eefbafe7af1416505
ameyM <ameym.dev@gmail.com>
2023-03-07 14:51:42 +0530

    added star for rating module, getlocation code added getting error

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,20 +12,39 @@ import cls from 'classnames'
 
 import SEO from '../components/seo'
 import { getCoffeeStores } from '../lib/coffee-store.js'
+import Link from 'next/link'
 
+
+const getCoffeeStoresNearBy = () => {
+
+    if(!navigator.geolocation) {
+        console.log('Browser donesn\'t  supported for Geolocation, Please try with next browser!')
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error)
+    }
+
+    function success(position){
+        let lat= position.coords.latitude
+        let long= position.coords.longitde
+        console.log(`${lat}: latitude, ${long}: longitute`)
+    }
+
+    function error(e) {
+        console.log(`opps! someting went wrong failed to locate your location! ${e}`)
+    }
+}
 
 /* pre-render content @build time using props returned by getStaticProps() */
 export async function getStaticProps() {
-
     /** coffee connouiser api configuration */
     let FSQData = await getCoffeeStores()
-
     return {
         props: {
             coffeeStores: FSQData
         }
     }
 }
+
 
 /**  client side  */
 export default function Home(props) {
@@ -34,27 +53,33 @@ let boxes = props.coffeeStores
         <div className={styles.container}>
             <main className={styles.main}>
                 <SEO 
-                title="coffeee connouiser app | Home"/>
+                title="coffeee connouiser app | Home"
+                />
                 <div className={styles.homeBannerWrapper}>
                     <div className={styles.left}>
                         <h1 className={styles.title}>
-                            <a href="#">
+                            <Link href="#">
                                 <span className={styles.w1}>Coffee</span> <br/>
                                 <span className={styles.w2}>Connoisseur</span>
-                            </a>
+                            </Link>
                         </h1>
                         <p 
                         className={styles.subTitle}
-                        >Discover your local coffee stores!</p>
+                        >
+                        Discover your local coffee stores!
+                        </p>
                     </div>
                     <div className={styles.right}>
-                        <button className={styles.discoverBtn}>Discover your local coffee stores</button>
+                        <button 
+                        className={styles.discoverBtn}
+                        onClick={getCoffeeStoresNearBy}
+                        >
+                            Discover your local coffee stores
+                        </button>
                     </div>
                 </div>
-
-
                 <div className='box'>
-                    <h2 className={CardStyle.boxName}>Coffee connouiser stores @ Dadar</h2>
+                    <h2 className={CardStyle.boxName}>The Coffee stores</h2>
                     <div className={cls(`text-center ${CardStyle.cardLayout}`)}>
                         {
                             boxes.map(box => {
